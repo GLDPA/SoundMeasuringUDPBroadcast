72fa32592c4c4ae7165aea791f33dc4c68572fe1
PeterKruhoffer <Peter.kruhoffer@gmail.com>
2018-05-09 12:51:32 +0200

    Insertdata lavet

--- SoundMeasuringUDPBroadcast/SoundMeasuringUDPBroadcast/Program.cs
+++ SoundMeasuringUDPBroadcast/SoundMeasuringUDPBroadcast/Program.cs
@@ -14,6 +14,7 @@ namespace SoundMeasuringUDPBroadcast
     {
         static void Main(string[] args)
         {
+            var pm = new Program();
             // laver en udpclient der kan læse på en specifik port
             UdpClient udpReceiver = new UdpClient(7000);
 
@@ -30,11 +31,14 @@ namespace SoundMeasuringUDPBroadcast
 
 
                     Byte[] receiveBytes = udpReceiver.Receive(ref RemoteIpEndPoint);
+                    Console.WriteLine("Received data");
 
                     string receivedData = Encoding.ASCII.GetString(receiveBytes);
+                    Console.WriteLine("Data processed");
+                    Console.WriteLine("Recorded Data" + receivedData);
                     if (receivedData.Equals("STOP.Secret")) throw new Exception("Receiver stopped");
 
-
+                    pm.InsertDataInDatabase(receivedData);
                 }
             }
             catch (Exception e)
@@ -42,22 +46,42 @@ namespace SoundMeasuringUDPBroadcast
                 Console.WriteLine(e);
                 throw;
             }
-
-
-            public int InsertData(string temprature)
-            {
-                var time = DateTime.Now.ToShortTimeString();
-            }
-            var sqlQuery = "INSERT INTO [dbo].[Measurments](ID, DECIBEL, DATE) VALUES (@ID, @DECIBEL, @DATE)";
-
-            using (var connection = new SqlConnection(connectionString))
-            {
-
-            }
-
+           
 
         }
 
+        
 
+        public int InsertDataInDatabase(string temprature)
+        {
+            var time = DateTime.Now.ToShortTimeString();
+            if (time == null || temprature == null)
+            {
+                throw new ArgumentException("Cannot insert null");
+            }
+
+            var connectionstring =
+                "Server=tcp:davids-sql-server.database.windows.net,1433;Initial Catalog=Davids sql server;Persist Security Info=False;User ID=davidmalmberg;Password=Dak/Tha12;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            using (var connection = new SqlConnection(connectionstring))
+            {
+                var sqlQuery = "INSERT INTO [dbo].[measurments] (Time,Temperatur, Humidity) VALUES(@Time, @Temperatur, @Humidity)";
+                using (var command = new SqlCommand(sqlQuery,connection))
+                {
+                    command.Parameters.AddWithValue("@Time", time);
+                    command.Parameters.AddWithValue("@Temprature", temprature);
+                    //command.Parameters.AddWithValue("@Humidity", humidity);
+
+                    connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    if (result < 0)
+                    {
+                        throw new ArgumentException("Nothing inserted");
+                    }
+                    return result;
+                }
+            }
+
+        }
+        
     }
 }
